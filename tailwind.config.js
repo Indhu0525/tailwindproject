37ea7c278ff16b01be2b562c43c8d8caec4af6ba
Indhu0525 <indu01401@hmail.com>
2024-07-02 12:41:32 +0530

    website

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'custom-black':'rgb(28 36 52)' , 
+        'text-menu':'rgb(138 153 175)',
+        'icon-white':'white',
+        'graydark':'rgb(51 58 72 )',
+        'primary':'rgb(60 80 224)',
+        'boxcolor':'rgb(36 48 63 )',
+        
+      },
+      
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none',
+        },
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none', /* IE and Edge */
+          'scrollbar-width': 'none', /* Firefox */
+        },
+      };
+
+      addUtilities(newUtilities);
+    },
+  ],
+}
+
